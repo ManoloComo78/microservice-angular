0d28cbeca61984d7bd4b2d3ab78619d24031227c
ManoloComo78 <Gabber1978>
2022-10-26 10:03:44 +0200

    Add dynamic component

--- src/app/user/users-select-model.ts
+++ src/app/user/users-select-model.ts
@@ -1,6 +1,6 @@
 import {DynamicOptionInterface} from "../dynamic-select/dynamic-option.interface";
 
 export class UsersSelectModel {
-  constructor(public title: string, public typeName: string, public data: DynamicOptionInterface[]) {
+  constructor(public title: string, public typeName: string, public options: DynamicOptionInterface[]) {
   }
 }
